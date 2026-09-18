37c8aa9599ddc9d7397343528977f4eca01a9f56
Max <max.zinke@googlemail.com>
2026-09-18 20:41:55 +0200

    Prepare fork launcher test release 0.6.2.1

--- netpunch/lobby.py
+++ netpunch/lobby.py
@@ -2371,7 +2371,7 @@ def _clear_stale_incoming(directory, log=_log):
 # --------------------------------------------------------------------------- #
 # PUBLISH: the OpenTTD-style public list (netpunch/masterserver.py)
 # --------------------------------------------------------------------------- #
-LOBBY_VERSION = "0.6.2"
+LOBBY_VERSION = "0.6.2.1"
 
 
 def version_rejection(remote):
